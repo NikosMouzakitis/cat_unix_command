950dc747ec86823fc33ad42f0f2d1b3a4eece7f6
NikosMouzakitis <mzktsn@gmail.com>
2017-12-08 19:30:57 +0200

    Create cat2.c

---
+++ cat2.c
@@ -0,0 +1,41 @@
+        #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#define SIZE 8
+
+int main(int argc,char *argv[])
+{
+        /*usage check*/
+  
+        if(argc != 2) {
+        printf("Usage error\n");
+        exit(-1);
+        }
+
+        int fd;
+        char *file;
+        char buffer[SIZE];
+        file = (char *) malloc(strlen(argv[1]) + 1);
+        int retval;
+        
+        if(file == NULL) {
+                printf("malloc failed\n");
+                exit(-1);
+        }
+        strcpy(file,argv[1]);
+        
+        if( ( fd = open(file,O_RDWR)) == -1) {
+                printf("/open failed\n");
+                exit(-1);
+       }
+        
+        while( ( retval = read(fd,buffer,SIZE) ) > 0 ) {
+                write(1,buffer,retval); //write in console.
+               
+        putchar('\n');
+        
+        return 0;
+}
